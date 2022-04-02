7e94669d05e98a9f8eb7def411a9b211a3bec991
Jonne Harja <tienkou@gmail.com>
2022-04-02 22:58:38 +0300

    UI ui ui ui

---
+++ LDJAM50/Assets/Scripts/UiController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiController : MonoBehaviour
+{
+
+    public void ShowGamePlayUI()
+    {
+        
+    }
+
+    public void ShowMainmenuUI()
+    {
+        
+    }
+
+    public void ShowGameOverUI()
+    {
+        
+    }
+
+    public void ShowSettingsUI()
+    {
+        
+    }
+     public void ShowShopUI()
+    {
+        
+    }
+}
