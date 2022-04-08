597bfdc7f5acd79433f1037ec62a8d83eff6dbad
Keith Davis <iamthevastidledhitchhiker@users.noreply.github.com>
2022-04-08 23:18:51 +0300

    Water hotfix

---
+++ LDJAM50/Assets/Shaders/WaterV2/FlagEnumAttribute.cs
@@ -0,0 +1,13 @@
+﻿#region Using statements
+
+using System;
+using UnityEngine;
+
+#endregion
+
+namespace WaterShader.Core
+{
+    public class FlagEnumAttribute : PropertyAttribute
+    {
+    }
+}
